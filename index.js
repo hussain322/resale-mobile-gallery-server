633bb908e140bda3345e863fb0c4eca642ecf2cb
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-25 20:27:25 +0600

    connet with mongodb and create category link

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z4ltr6s.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoryCollection = client
+      .db("resaleMarket")
+      .collection("categories");
+
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const options = await categoryCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+run().catch(console.log());
+
 app.get("/", (req, res) => {
-  res.send("resale server is running...");
+  res.send("resale server is running");
 });
 
 app.listen(port, () => {
