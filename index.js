e279b05b87f5b03ada58c7b2900d5145864ff6c2
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-25 20:23:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+const app = express();
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("resale server is running");
+});
+
+app.listen(port, () => {
+  console.log(`resale server is running on ${port}`);
+});
