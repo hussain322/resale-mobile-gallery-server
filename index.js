dd83c0ded9fd8f961eb9eb4fe3c93efb98079062
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-30 08:51:40 +0600

    fix some error

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 // middle ware
